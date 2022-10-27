a1e3c2468ecc567f12da7c0d79dbf7541596c783
Tan837 <115802784+Tan837@users.noreply.github.com>
2022-10-27 10:47:41 -0400

    Update PythonActivity.java

--- pythonforandroid/bootstraps/sdl2/build/src/main/java/org/kivy/android/PythonActivity.java
+++ pythonforandroid/bootstraps/sdl2/build/src/main/java/org/kivy/android/PythonActivity.java
@@ -399,7 +399,7 @@ public class PythonActivity extends SDLActivity {
                         }
                     };
                     loadingScreenRemovalTimer = new Timer();
-                    loadingScreenRemovalTimer.schedule(removalTask, 5000);
+                    loadingScreenRemovalTimer.schedule(removalTask, 15000);
                 }
             }
         });
